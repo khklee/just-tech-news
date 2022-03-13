fd32c983ada224e48f475608a0b4b6d449f76005
Kelly Lee <amorfati38@gmail.com>
2022-03-12 20:09:06 -0700

    add homepage

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -86,7 +86,7 @@ router.post('/', (req, res) => {
 
 // Put /api/posts/upvote
 router.put('/upvote', (req, res) => {
-    Post.upvote(req.body, { Vote })
+    Post.update(req.body, { Vote })
         .then(updatedPostData => res.json(updatedPostData))
         .catch(err => {
             console.log(err);
